c001c32d08a4f6a7b211ed93b2ca257f6cf4e2de
Alberto <asoler@topdoctors.es>
2020-03-09 15:51:44 +0100

    Refactorized store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,17 +1,27 @@
 // store.js
 import React, {createContext, useReducer} from 'react';
 
+const key = "habitissimo_budget"
+
+const getItemLocalStorage = (key, value, ret) => {
+  return localStorage.getItem(key) && (JSON.parse(localStorage.getItem(key))[value] || ret)
+}
+
+const setItemLocalStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value))
+}
+
 
 const initialState = {
-  step: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).step || 0),
-  description: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).description || ''),
-  date: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).date || ''),
-  category: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).category || ''),
-  subcategory: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).subcategory || ''),
-  price: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).price || ''),  
-  name: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).name || ''),
-  email: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).email || ''),
-  phone: localStorage.getItem("habitissimo_budget") && (JSON.parse(localStorage.getItem("habitissimo_budget")).phone || ''),
+  step: getItemLocalStorage(key, "step", 0) || 0,
+  description: getItemLocalStorage(key, "description", "") || "",
+  date: getItemLocalStorage(key, "date", ""),
+  category: getItemLocalStorage(key, "category", ""),
+  subcategory:getItemLocalStorage(key, "subcategory", "") || "",
+  price: getItemLocalStorage(key, "price", "") || "",  
+  name: getItemLocalStorage(key, "name", "") || "",
+  email: getItemLocalStorage(key, "email", "") || "",
+  phone: getItemLocalStorage(key, "phone", "") || "",
 };
 const store = createContext(initialState);
 const { Provider } = store;
@@ -20,34 +30,34 @@ const StateProvider = ( { children } ) => {
   const [state, dispatch] = useReducer((state, action) => {
     switch(action.type) {
       case 'step':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, step: action.value}));
+        setItemLocalStorage(key, {...state, step: action.value});
         return { ...state, step: action.value }
       case 'description':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, description: action.value}));
+        setItemLocalStorage(key, {...state, description: action.value});
         return { ...state, description: action.value }
       case 'date':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, date: action.value}));
+        setItemLocalStorage(key, {...state, date: action.value});
         return { ...state, date: action.value }
       case 'category':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, category: action.value}));
+        setItemLocalStorage(key, {...state, category: action.value});
         return { ...state, category: action.value }
       case 'subcategory':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, subcategory: action.value}));
+        setItemLocalStorage(key, {...state, subcategory: action.value});
         return { ...state, subcategory: action.value }
       case 'price':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, price: action.value}));
+        setItemLocalStorage(key, {...state, price: action.value});
         return { ...state, price: action.value }
       case 'name':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, name: action.value}));
+        setItemLocalStorage(key, {...state, name: action.value});
         return { ...state, name: action.value }
       case 'email':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, email: action.value}));
+        setItemLocalStorage(key, {...state, email: action.value});
         return { ...state, email: action.value }
       case 'phone':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({...state, phone: action.value}));
+        setItemLocalStorage(key, {...state, phone: action.value});
         return { ...state, phone: action.value }
       case 'clear':
-        localStorage.setItem("habitissimo_budget", JSON.stringify({}));
+        setItemLocalStorage(key, {});
         return { 
           step: 0,
           description: "",
